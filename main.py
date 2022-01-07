b5418143df6c8449d54f44353502076a34c00ac5
Christian F <96458958+cfeliciano7792@users.noreply.github.com>
2022-01-07 03:10:24 +0000

    Added last 3 if/else statements

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ if bmi < 18.5:
     
 elif bmi < 25:
     print(f"Your BMI is {bmi}, you have a normal weight.")
+
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+
+else:
+    print(f"You BMI is {bmi}, you are clinically obese.")
