15e029ce21e0ee21ddfb13b2851f204ecea0aec9
Christian F <96458958+cfeliciano7792@users.noreply.github.com>
2022-01-07 03:09:44 +0000

    Added first 2 if/else statement

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ weight = float(input("enter your weight in kg: "))
 #calculates BMI
 bmi = round((weight / (height**2)))
 
+#If/else statements to determine which condition is true
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+    
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
